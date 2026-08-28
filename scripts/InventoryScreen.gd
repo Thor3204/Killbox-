a03e288e5da71886d4a63f82829fa0e0907250ca
Thor3204 <kronos.elgranfuturo@gmail.com>
2026-08-28 07:59:34 -0400

    chore: remove legacy Godot project (scripts)

--- scripts/InventoryScreen.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-class_name InventoryScreen
-signal back_pressed
-func _ready():
- set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
- var l=Label.new(); l.text="INVENTARIO\n\n👕 ROPA\n🔫 ARMAS\n🎒 EQUIPO\n🧩 OBJETOS\n\nAquí aparecerán tus objetos y equipamiento."; l.position=Vector2(70,70); l.add_theme_font_size_override("font_size",30); add_child(l)
- var b=Button.new(); b.text="← VOLVER"; b.position=Vector2(70,600); b.size=Vector2(190,60); b.pressed.connect(func():back_pressed.emit()); add_child(b)
